ddc5ea1438e1231352c0737b9f68ee10934b1d1f
diegodestf <diegodesteffano@gmail.com>
2022-05-30 13:59:09 -0300

    Refactored for ES6

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-function Book(title, author, isbn) {
-    this.title = title
-    this.author = author
-    this.isbn = isbn
+// ES6
+
+class Book {
+
+    constructor(title, author, isbn) {
+
+        this.title = title
+        this.author = author
+        this.isbn = isbn
+    }
 }
 
-const form = document.querySelector('#book-form')
-const bookList = document.querySelector('#book-list')
+class UI {
 
-function UI() {}
+    addBookToList(book) {
 
-UI.prototype.addBookToList = (book) => {
     const list = document.querySelector('#book-list')
 
     const row = document.createElement('tr')
@@ -20,9 +24,11 @@ UI.prototype.addBookToList = (book) => {
                     <td><a href="" class="delete">X<a></td>`
 
     list.appendChild(row)
-}
 
-UI.prototype.showAlert = (message, className) => {
+    }
+
+    showAlert(message, className) {
+
     const div = document.createElement('div')
 
     div.className = `alert ${className}`
@@ -38,19 +44,25 @@ UI.prototype.showAlert = (message, className) => {
         document.querySelector('.alert').remove()
     }, 3000);
 
-}
+    }
 
-UI.prototype.deleteBook = (target) => {
-    if (target.className === 'delete') {
+    deleteBook(target) {
+
+        if (target.className === 'delete') {
         target.parentElement.parentElement.remove()
+    }
+
+    }
+
+    clearFields() {
+    document.querySelector('#title').value = ''
+    document.querySelector('#author').value = ''
+    document.querySelector('#isbn').value = ''
     }
 }
 
-UI.prototype.clearFields = () => {
-    document.querySelector('#title').value = ''
-    author = document.querySelector('#author').value = ''
-    isbn = document.querySelector('#isbn').value = ''
-}
+const form = document.querySelector('#book-form')
+const bookList = document.querySelector('#book-list')
 
 form.addEventListener('submit', (e) => {
     const title = document.querySelector('#title').value,
@@ -83,3 +95,95 @@ bookList.addEventListener('click', (e) => {
 
     e.preventDefault()
 })
+
+
+
+
+// ES5
+
+
+// function Book(title, author, isbn) {
+//     this.title = title
+//     this.author = author
+//     this.isbn = isbn
+// }
+
+// const form = document.querySelector('#book-form')
+// const bookList = document.querySelector('#book-list')
+
+// function UI() {}
+
+// UI.prototype.addBookToList = (book) => {
+//     const list = document.querySelector('#book-list')
+
+//     const row = document.createElement('tr')
+
+//     row.innerHTML = `<td>${book.title}</td>
+//                     <td>${book.author}</td>
+//                     <td>${book.isbn}</td>
+//                     <td><a href="" class="delete">X<a></td>`
+
+//     list.appendChild(row)
+// }
+
+// UI.prototype.showAlert = (message, className) => {
+//     const div = document.createElement('div')
+
+//     div.className = `alert ${className}`
+
+//     div.appendChild(document.createTextNode(message))
+
+//     const container = document.querySelector('.container')
+//     const form = document.querySelector('#book-form')
+
+//     container.insertBefore(div, form)
+
+//     setTimeout(() => {
+//         document.querySelector('.alert').remove()
+//     }, 3000);
+
+// }
+
+// UI.prototype.deleteBook = (target) => {
+//     if (target.className === 'delete') {
+//         target.parentElement.parentElement.remove()
+//     }
+// }
+
+// UI.prototype.clearFields = () => {
+//     document.querySelector('#title').value = ''
+//     document.querySelector('#author').value = ''
+//     document.querySelector('#isbn').value = ''
+// }
+
+// form.addEventListener('submit', (e) => {
+//     const title = document.querySelector('#title').value,
+//           author = document.querySelector('#author').value,
+//           isbn = document.querySelector('#isbn').value
+
+//     const book = new Book(title, author, isbn)
+
+//     const ui = new UI()
+
+//     if (title === '' || author === '' || isbn === '') {
+//         ui.showAlert('Please fill in all the fields', 'error')
+
+//     } else {
+//         ui.addBookToList(book)
+//         ui.showAlert('Book added', 'success')
+//     }
+
+//     ui.clearFields()
+//     e.preventDefault()
+// })
+
+// bookList.addEventListener('click', (e) => {
+
+//     const ui = new UI
+
+//     ui.deleteBook(e.target)
+
+//     ui.showAlert('Book removed!', 'success')
+
+//     e.preventDefault()
+// })
